884105532670f1d9efdc0d807e5eaaf5dc29add2
Raul Montan Rodriguez <raulrodriguez1992@icloud.com>
2017-10-17 14:24:02 -0400

    Inicio de Proyecto SM

--- Store_management/MainWindow.xaml.cs
+++ Store_management/MainWindow.xaml.cs
@@ -12,13 +12,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Behaviours;
 
 namespace Store_management
 {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
