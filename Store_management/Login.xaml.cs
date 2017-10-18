cae3e0331ceea79f9d616fc5e1dd23468289be03
Raul Montan Rodriguez <raulrodriguez1992@icloud.com>
2017-10-17 20:14:26 -0400

    Agregado de la ventanas: Login y Register

---
+++ Store_management/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Behaviours;
+
+namespace Store_management
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : MetroWindow
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
